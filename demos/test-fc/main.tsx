e783486867e035bcb9a8fef5affd950afe2122f1
CeazzZY <1093918683@qq.com>
2024-01-28 22:09:48 +0800

    feat: vite config

---
+++ demos/test-fc/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+
+function App() {
+	return (
+		<div>
+			<span>222</span>
+		</div>
+	);
+}
